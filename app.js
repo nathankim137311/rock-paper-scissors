ca761eb3d5ab1e747b0d9f59ed8ccc5eb629dece
web-dev <nathankim137311@gmail.com>
2021-09-06 23:06:06 -0700

    completed basic rock paper scissors game

--- app.js
+++ app.js
@@ -1,28 +1,49 @@
-console.log('hello everybody!');
+console.log('hello everyone'); 
 
-//Variables 
-const displayYourResult = document.getElementById('user-result');
-const displayComputerResult = document.getElementById('computer-result');
-const displayGameResult = document.getElementById('game-result');
-const rockBtn = document.getElementById('rock');
-const paperBtn = document.getElementById('paper');
-const scissorsBtn = document.getElementById('scissors');
-const choiceBtns = document.querySelectorAll('.btn');
-//Eventlisteners
-//Functions 
-choiceBtns.forEach(element => console.log(element));
+// variables
+// a game prompt window pops up 
+const playerSelection = prompt('Rock Paper or Scissors?').toLowerCase(); 
+const computerSelection = computerPlay(); 
+// event listeners
 
+// functions
 
+// random number from 1 - 3 generator printing rock paper and scissors
+function computerPlay () {
+  let randomNumber = Math.floor(Math.random()*3) + 1;
+  if (randomNumber == 1) {
+    return('rock'); 
+  } else if (randomNumber == 2) {
+    return('paper');
+  } else {
+    return('scissors'); 
+  }
+}
+// a function determining the winner of a round 
+function oneRound (playerSelection, computerSelection) {
 
+  playerSelection.toLowerCase; 
+  computerSelection.toLowerCase; 
 
-/* 
-//normal function syntax 
-document.addEventListener('click', function () {
-  console.log('Click')
-})
-//arrow function syntax
-document.addEventListener('click', () => console.log('Click'))
+  if (playerSelection == 'rock' && computerSelection == 'paper') {
+    console.log('Paper beats rock, computer wins!'); 
+  } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+    console.log('Rock beats scissors, user wins!');
+  } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+    console.log('Paper beats rock, user wins!'); 
+  } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+    console.log('Scissors beats paper, computer wins!');
+  } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+    console.log('Rock beats scissors, user wins!'); 
+  } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+    console.log('Scissors beats paper, user wins!'); 
+  } else if (playerSelection === computerSelection) {
+    console.log('It\'s a tie!'); 
+  }
+}
 
-//These functions work the same
-*/
+//printing player, computer and game results
+console.log('user:' + playerSelection);
+console.log('computer:' + computerSelection);
+(oneRound(playerSelection, computerSelection)); 
 
